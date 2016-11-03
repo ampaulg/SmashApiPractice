865eca554f051fbbcd137eadc02b6dff3526fadd
ampaulg <ampaulgill@gmail.com>
2016-11-02 19:08:16 -0700

    Selection and specials
    Now uses a select box with values from API, also prints special move list for that character.

--- kuroTest.js
+++ kuroTest.js
@@ -1,14 +1,47 @@
-//gets name from id
-//takes id from "entry" box
-//returns name in "results1"
-function getName(){
+function makeSelect(){
+	
+	//get teh JSON object containing all the characters and IDs
+	var str1 = "http://api.kuroganehammer.com/api/Characters?fields=name%2Cid";
+	var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", str1, false );
+    xmlHttp.send( null );
+    var characterArr = JSON.parse(xmlHttp.responseText);
+    var length = characterArr.length;
+
+    //make the selection box
+    var str2 = '<select id="entry">';
+    var str3;
+    for (i=0; i<length;i++){
+    	str2 = str2.concat('<option value=');
+    	str3 = characterArr[i].id.toString();
+    	str2 = str2.concat(str3);
+        str2 = str2.concat('>');
+        str2 = str2.concat(characterArr[i].name);
+        str2 = str2.concat("</option>");
+    }
+    str2 = str2.concat("</select>");
+    
+    var element = document.getElementById("selectBox");
+	element.innerHTML = str2;
+}
+
+
+//gets name and special moves from id
+//takes id from selection box
+//returns name in results1
+//		  number of moves in results2
+//		  list of specials in results3
+//		  the full JSON text in results4
+function getInfo(){
 
 	var element = document.getElementById("results1");
-	var entry = document.getElementById("entry").value;
-
+	var select = document.getElementById("entry");
+    var entry = select.options[select.selectedIndex].value;
+    element.innerHTML = entry;
 	//check if input is in the right range
 	if ((entry>58)||(entry<1)){
-		element.innerHTML = "Invalid number";
+		//element.innerHTML = "Invalid number";
+		//element.innerHTML = entry;
 		return;
 	}
 
@@ -27,4 +60,34 @@ function getName(){
 
     //get the name from the JSON object
 	element.innerHTML = JSON.parse(xmlHttp.responseText).name;
+
+	element = document.getElementById("results2");
+
+	str1 = "http://api.kuroganehammer.com/api/Characters/";
+	str3 = "/moves?fields=name%2Ctype%2ChitboxActive%2CfirstActionableFrame%2CbaseDamage%2C";
+
+	str1 = str1.concat(str2);
+	str1 = str1.concat(str3);
+
+	//get value from API
+	var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", str1, false );
+    xmlHttp.send( null );
+
+    var moveArr = JSON.parse(xmlHttp.responseText);
+    var length = moveArr.length;
+	element.innerHTML = length.toString();
+	element = document.getElementById("results4");
+	element.innerHTML = xmlHttp.responseText;
+	
+	element = document.getElementById("results3");
+	
+	var out = "";
+	for (i=0; i<length;i++){
+        if (moveArr[i].type==2){
+			out = out.concat(moveArr[i].name);
+			out = out.concat("<br/>");
+	    }
+	}
+	element.innerHTML = out;
 }
